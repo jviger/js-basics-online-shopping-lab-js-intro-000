63b837e743fde7557327387622c2edb820b24e8b
Jason Viger <jason.viger@gmail.com>
2018-07-10 05:42:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,8 +64,10 @@ function placeOrder(cardNumber) {
   if(!cardNumber){
   return "Sorry, we don't have a credit card on file for you.";
   }else {
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+    var cartTotal = total();
     cart = [];
+    return `Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`;
+    
   }
 }
 
