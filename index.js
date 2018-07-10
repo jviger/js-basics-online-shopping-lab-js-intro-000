ab6e824708992180285bccd8d1156da1a82c929f
Jason Viger <jason.viger@gmail.com>
2018-07-10 05:29:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,13 @@ function viewCart() {
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
   } else if (cart.length === 2){
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.` ;
-  }
+  } else {
+    var message = `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, ${cart[1].itemName} at $${cart[1].itemPrice}, ` ;
+    for(let i = 2; i < cart.length -1; i++){
+       message = message + `${cart[i].itemName} at $${cart[i].itemPrice}, `;
+      
+    }
+}
 }
 
 function total() {
